19583873349daa43a0e8c0e723d2b2138cd12324
ashirbad <ashirbadbehera29@gmail.com>
2022-03-10 23:59:17 +0530

    🚨  linting setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-	return (
-		<section className='container'>
-			<h1>Hi Mom!</h1>
-		</section>
-	);
+  return (
+    <section className="container mt-3 pb-3">
+      <h1>Hi Mom!</h1>
+    </section>
+  );
 };
 
 export default Home;
